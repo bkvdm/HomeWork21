3b39ee8066f63ef4687138a30cb562bb5a7805d7
bkvdm <vadim@991199.ru>
2023-10-19 21:01:26 +0300

    Домашнее задание №21 (Коллекции: сеты и мапы) https://skyengpublic.notion.site/2-6-b7981b38506b4066915285a4d789122f

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.lang.*;
+import java.util.Set;
+import java.util.Map;
 
 public class Main {
 
@@ -31,10 +34,26 @@ public class Main {
         return numberSeriesList;
     }
 
+    public static List<String> listRandomString() {
+        java.util.Random random = new java.util.Random();
+//        Random random = new Random();
+        List<String> words = new ArrayList<>(Arrays.asList("Автомобиль", "Автобус", "Велосипед", "Грузовик", "Мотоцикл", "Поезд", "Самолет", "Корабль", "Метро", "Такси", "Фургон", "Лодка", "Скутер", "Плот", "Верблюд", "Собака", "Кошка", "Лев", "Тигр", "Гепард", "Леопард", "Ягуар", "Пантера", "Рысь", "Волк", "Медведь", "Ежик", "Заяц", "Кролик", "Белка", "Мышь", "Крыса", "Кенгуру", "Жираф", "Зебра", "Антилопа", "Олень", "Лось", "Марал", "Кабан", "Баран", "Овца", "Коза", "Свинья", "Петух", "Курица", "Утка", "Гусь", "Страус", "Павлин", "Фазан", "Голубь", "Воробей", "Канарейка", "Попугай", "Жако", "Амазон", "Сорока", "Ворона", "Ворон", "Чайка", "Гагара", "Альбатрос", "Пеликан", "Цапля", "Аист", "Фламинго", "Лебедь", "Гусь", "Утенок", "Цыпленок", "Щенок", "Котенок", "Теленок", "Козленок", "Поросенок", "Жеребенок", "Ягненок", "Козленок", "Цыпленок", "Гусененок", "Кенгуренок", "Страусеннок", "Головастик", "Лягушонок", "Черепаха", "Ящерица", "Хамелеон", "Уж", "Змея", "Кобра", "Гадюка", "Ехидна", "Мангуст", "Суслик", "Хомяк", "Бурундук", "Сурок", "Дикобраз", "skypro"));
+        int minimumScore = 1;
+        int iteratorRandom = random.nextInt(words.size()) + minimumScore;
+        int minimumIndex = 0;
+        List<String> wordList = new ArrayList<>();
+        for (int i = 0; i < iteratorRandom; i++) {
+            int iteratorIndex = random.nextInt(words.size()) + minimumIndex;
+            wordList.add(words.get(iteratorIndex));
+        }
+        return wordList;
+    }
+
     public static void main(String[] args) {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1() {
@@ -75,6 +94,35 @@ public class Main {
 //    }
     public static void task3() {
         System.out.println("Задача 3");
-
+        List<String> stringList = listRandomString();
+        Set<String> uniqueWords = new HashSet<>();
+        uniqueWords.addAll(stringList);
+        System.out.println(stringList);
+        System.out.println(uniqueWords);
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        List<String> stringList = listRandomString();
+        System.out.println(stringList);
+        Map<String, Integer> stringMap = new HashMap<>();
+        Set<String> uniqueWords = new HashSet<>();
+        for (String s : stringList) {
+            if (stringMap.get(s) == null) {
+                stringMap.put(s, 1);
+            } else {
+                stringMap.put(s, stringMap.get(s) + 1);
+            }
+        }
+        uniqueWords.addAll(stringList);
+        System.out.println(stringMap);
+        for (String uniqueWord : uniqueWords) {
+            System.out.println(stringMap.get(uniqueWord));
+        }
+//        for (Integer i = 0; i < stringMap.get(i); i++) {
+//            
+//        }
+//        for (int i = 0; i < stringMap.size(); i++) {
+//            stringMap.get(i);
+//        }
     }
 }
